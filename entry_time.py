6d57c608159d159d37723f6e08f78380454b0756
WaqasAkbarEngr <68653306+WaqasAkbarEngr@users.noreply.github.com>
2021-05-22 18:28:53 +0500

    Delete entry_time.py

--- entry_time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def entry_time():
-    entry = datetime.datetime.now()
-    time_min = entry.strftime("%M")
-    time_hour = entry.strftime("%H")
-    print(time_hour+time_min)
-    print("You Entered at "+entry.strftime("%H")+":"+entry.strftime("%M"))
